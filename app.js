b2979edbee9c5e21a75c5acbeb9e43e69b07a4a0
Comision19 <cristianbeliera@gmail.com>
2023-01-27 20:53:44 -0300

    First commit TP Mercado Liebre parte VI

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3050;
+
+app.use(express.static('public'));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+})
+
+app.listen(PORT, () =>
+    console.log(`Server listen in port ${PORT}\n http://localhost:${PORT}`));
